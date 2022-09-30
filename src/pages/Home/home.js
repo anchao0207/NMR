9ac1db75c88876f21ef070778376533a00171602
An <atrieu@knox.edu>
2022-09-30 17:57:19 -0500

    add in background and 2 first component

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,12 +1,19 @@
 import React from "react";
+import "./home.css"
 import { useViewport } from "../../viewportContext";
+import {ReactComponent as NuclearMagneticResonance} from './NuclearMagneticResonance.svg';
+import {ReactComponent as PlayButton} from './playButton.svg';
 
 export default function Home() {
   const { width, height } = useViewport();
   return (
-    <>
-      <span>width is: {width} </span>
-      <span>height is: {height} </span>
-    </>
+    <div className="home">
+      <div className="section1">
+      <NuclearMagneticResonance className="NMR"/>
+      </div>
+      <div className="section2">
+      <PlayButton className="play-button"/>
+      </div>
+    </div>
   );
 }
