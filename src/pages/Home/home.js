4b9293231677ac1b85fef0300aafcf0efb414421
T <mnle@knox.edu>
2022-10-02 21:05:43 -0500

    update 10/2

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { useViewport } from "../../viewportContext";
-import Main from "../../components/Main/main"
 
 export default function Home() {
   const { width, height } = useViewport();
   return (
     <>
-      <Main></Main>
       <span>width is: {width} </span>
       <span>height is: {height} </span>
     </>
