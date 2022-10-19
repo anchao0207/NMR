f6f61c3e674bce98c8c8ec00fc56b2b037f50fe5
jessie <huquach@knox.edu>
2022-10-18 19:05:59 -0500

    Fixed navigation between different pages

--- src/pages/NMRFundamentals/nmrFundamentals.js
+++ src/pages/NMRFundamentals/nmrFundamentals.js
@@ -4,11 +4,22 @@ import { useViewport } from "../../viewportContext";
 import { ReactComponent as NuclearMagneticResonance } from "../Home/NuclearMagneticResonance.svg";
 import NavBar from "../../components/NavBar/navbar1";
 import Stack from 'react-bootstrap/Stack';
+import { useNavigate } from "react-router-dom";
 
 export default function NMRFundamentals() {
     const { width, height } = useViewport();
     const content = ["How it Works", "NMR\nInstrumentation", "Intepreting\nNMR Spectrum"];
     
+    let navigate = useNavigate();
+    const handleClick = (e) => {
+        if(e = "How it Works"){
+            let path = "http://localhost:3000/";
+            navigate(path);
+        }
+        path = "http://localhost:3000/StillDeveloping/";
+        navigate(path);
+    }
+
     return (
         <>
         <div className="home">
@@ -20,6 +31,7 @@ export default function NMRFundamentals() {
                 <div className="section2">
                     <Stack gap={3}>
                         {content.map((o) => {
+                            // <button onClick={handleClick(o)}>{o}</button>
                             if(o === "How it Works") 
                                 return <a href="http://localhost:3000/"><button>{o}</button></a>
                             
