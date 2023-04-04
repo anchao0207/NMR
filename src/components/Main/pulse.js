4861bb2c641897c50aa123da014c91f63c54e41e
An <atrieu@knox.edu>
2023-04-04 11:00:26 -0500

    add light click back

--- src/components/Main/pulse.js
+++ src/components/Main/pulse.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios"
 import "./main.css";
 
 const content = {
@@ -15,6 +16,35 @@ const content = {
     "P {109 MHz}"
 };
 
+const onHClick = (e) => {
+  axios({
+    method: "POST",
+    url: "http://localhost:8080/pulseH",
+  })
+  e.preventDefault();
+}
+const onCClick = (e) => {
+  axios({
+    method: "POST",
+    url: "http://localhost:8080/pulseC",
+  })
+  e.preventDefault();
+}
+const onFClick = (e) => {
+  axios({
+    method: "POST",
+    url: "http://localhost:8080/pulseF",
+  })
+  e.preventDefault();
+}
+const onPClick = (e) => {
+  axios({
+    method: "POST",
+    url: "http://localhost:8080/pulseP",
+  })
+  e.preventDefault();
+}
+
 const MainBody = (props) => {
   return (
     <div className="container">
@@ -28,10 +58,10 @@ const MainBody = (props) => {
             </p>
             <div className="center">
               <p>
-                <a className="link"><sup>1</sup>{props.link1}</a>
-                <a className="link"><sup>13</sup>{props.link2}</a>
-                <a className="link"><sup>19</sup>{props.link3}</a>
-                <a className="link"><sup>31</sup>{props.link2}</a>
+                <a className="link" onClick={onHClick}><sup>1</sup>{props.link1}</a>
+                <a className="link" onClick={onCClick}><sup>13</sup>{props.link2}</a>
+                <a className="link" onClick={onFClick}><sup>19</sup>{props.link3}</a>
+                <a className="link" onClick={onPClick}><sup>31</sup>{props.link2}</a>
               </p>
             </div>
         </div>
--- src/components/Main/temperature.js
+++ src/components/Main/temperature.js
@@ -1,22 +1,42 @@
 import React from "react";
 import "./main.css";
+import axios from "axios";
 
 const content = {
   heading: "The\nTemperature",
   description1:
     "In order for our magnet to be superconducting it needs to be at a very cold temperature.\n\nThe superconducting magnet is cooled by ",
-  link1:
-    "liquid helium",
+  link1: "liquid helium",
   description2:
     ", which provides this low temperature (4K / -452℉/ -269℃).\n\nThe liquid helium will evaporate over time. To slow down this evaporation, the (expensive) liquid helium is kept cold by a separate layer of (cheap) ",
-  link2:
-    "liquid nitrogen",
+  link2: "liquid nitrogen",
   description3:
     " (77K, -196℃ / -320℉).\n\nThe outside layer of the can is insulated by a vacuum and with ",
-  link3:
-    "Mylar",
+  link3: "Mylar",
   description4:
-    ", to help keep the magnet cold.\n\nBasically, the entire metal can is like a Yeti - an excellent thermos!"
+    ", to help keep the magnet cold.\n\nBasically, the entire metal can is like a Yeti - an excellent thermos!",
+};
+
+const onHeliClick = (e) => {
+  axios({
+    method: "POST",
+    url: "http://localhost:8080/temperature-heli",
+  });
+  e.preventDefault();
+};
+const onNitroClick = (e) => {
+  axios({
+    method: "POST",
+    url: "http://localhost:8080/temperature-nitro",
+  });
+  e.preventDefault();
+};
+const onMylarClick = (e) => {
+  axios({
+    method: "POST",
+    url: "http://localhost:8080/temperature-mylar",
+  });
+  e.preventDefault();
 };
 
 const MainBody = (props) => {
@@ -27,9 +47,15 @@ const MainBody = (props) => {
       </div>
       <div className="description">
         <div className="decs-text">
-            <p>
-            {props.description1}<a className="link">{props.link1}</a>{props.description2}<a className="link">{props.link2}</a>{props.description3}<a className="link">{props.link3}</a>{props.description4}
-            </p>
+          <p>
+            {props.description1}
+            <a className="link" onClick={onHeliClick}>{props.link1}</a>
+            {props.description2}
+            <a className="link" onClick={onNitroClick}>{props.link2}</a>
+            {props.description3}
+            <a className="link" onClick={onMylarClick}>{props.link3}</a>
+            {props.description4}
+          </p>
         </div>
       </div>
     </div>
