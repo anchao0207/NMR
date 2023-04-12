5b609ddb50970036230312a853dafb7a032580b4
An <atrieu@knox.edu>
2023-04-11 23:07:41 -0500

    fixing layout

--- src/components/Main/technology.js
+++ src/components/Main/technology.js
@@ -1,23 +1,11 @@
 import React from "react";
 import "./main.css";
+import MainBody from "../CourseUsage/MainBody"
 
 const content = {
   heading: "The\nTechnology",
   description:
     "Have you ever had an MRI?\n\nNMR spectroscopy uses the same technology.\n\nParticular nuclei, magnetic fields, and radio waves are combined to study the environments at the atomic/molecular level.\n\nAn MRI specifically looks at the hydrogen nuclei in water molecules in your bodily tissues to produce an image that doctors use to understand what’s happening in your body.\n\nNMR spectroscopy uses the same technology.  Chemists use an NMR spectrum to understand the chemical structure of a molecule.",
-};
-
-const MainBody = (props) => {
-  return (
-    <div className="container">
-      <div className="header">
-        <div className="head-text">{props.heading}</div>
-      </div>
-      <div className="description">
-        <div className="decs-text"><p>{props.description}</p></div>
-      </div>
-    </div>
-  );
 };
 
 const BodyFirstPage = () => {
