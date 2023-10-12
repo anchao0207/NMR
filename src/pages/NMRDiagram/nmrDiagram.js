1fa74be42cfebf5e5ca82e82e59751738c6abef8
An <atrieu@knox.edu>
2023-10-12 12:37:00 -0500

    adjusting bad layout from the base and add in light function

--- src/pages/NMRDiagram/nmrDiagram.js
+++ src/pages/NMRDiagram/nmrDiagram.js
@@ -4,6 +4,7 @@ import "./nmrDiagram.css";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Stack from "react-bootstrap/Stack";
+import axios from "axios";
 //import { ReactComponent as Diagram } from "../../assets/NMRDiagramPlaceholder.png";
 
 export default function NMRDiagram() {
@@ -18,6 +19,119 @@ export default function NMRDiagram() {
 
   const mainRef = React.useRef(null);
 
+  const onMagnetClick = async (e) => {
+    try {
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/off",
+      });
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/magnet",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    e.preventDefault();
+  };
+
+  const onHeliClick = async (e) => {
+    try {
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/off",
+      });
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/temperature-heli",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    e.preventDefault();
+  };
+
+  const onNitroClick = async (e) => {
+    try {
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/off",
+      });
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/temperature-nitro",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    e.preventDefault();
+  };
+
+  const onMylarClick = async (e) => {
+    try {
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/off",
+      });
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/temperature-mylar",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    e.preventDefault();
+  };
+
+  const onSampleClick = async (e) => {
+    try {
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/off",
+      });
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/sample",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    e.preventDefault();
+  };
+
+  const onFieldClick = async (e) => {
+    try {
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/off",
+      });
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/field",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    e.preventDefault();
+  };
+
+  const onHClick = async (e) => {
+    try {
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/off",
+      });
+      await axios({
+        method: "POST",
+        url: "http://localhost:8080/pulseH",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  
+    e.preventDefault();
+  };
+
   return (
     <>
       <div className="home">
@@ -25,18 +139,21 @@ export default function NMRDiagram() {
         <div className="navTitle" ref={titleRef}>
           <span>Diagram of Components</span>
         </div>
-
+        <div className="diagram">
+          
         <img className="image" src={require("../../assets/NMRDiagramPlaceholder.png")} alt="" />
+        </div>
   
-        <Stack className="section2">
-            <Link to="/NMRDiagram"><button>The Magnet</button></Link>
-            <Link to="/NMRDiagram"><button>Liquid Helium</button></Link>
-            <Link to="/NMRDiagram"><button>Liquid Nitrogen</button></Link>
-            <Link to="/NMRDiagram"><button>Mylar</button></Link>
-            <Link to="/NMRDiagram"><button>The Sample</button></Link>
-            <Link to="/NMRDiagram"><button>Shim Coils</button></Link>
-            <Link to="/NMRDiagram"><button>The Pulse</button></Link>
+        <Stack className="section2 button-list">
+            <Link to="/NMRDiagram"><button onClick={onMagnetClick}>The Magnet</button></Link>
+            <Link to="/NMRDiagram"><button onClick={onHeliClick}>Liquid Helium</button></Link>
+            <Link to="/NMRDiagram"><button onClick={onNitroClick}>Liquid Nitrogen</button></Link>
+            <Link to="/NMRDiagram"><button onClick={onMylarClick}>Mylar</button></Link>
+            <Link to="/NMRDiagram"><button onClick={onSampleClick}>The Sample</button></Link>
+            <Link to="/NMRDiagram"><button onClick={onFieldClick}>Shim Coils</button></Link>
+            <Link to="/NMRDiagram"><button onClick={onHClick}>The Pulse</button></Link>
         </Stack>
+
       </div>
     </>
   )
